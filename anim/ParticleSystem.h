03bd644275ea811f9256230e0c5f5243a45c1cb9
Cobey Hollier <cobeyhollier@uvic.ca>
2021-10-10 17:01:29 -0700

    CH: Adding missing file

--- anim/ParticleSystem.h
+++ anim/ParticleSystem.h
@@ -3,17 +3,28 @@
 #include <vector>
 
 struct Particle {
+	Particle(Vector initalPos, Vector initalVel, double particleMass) {
+		VecCopy(position, initalPos);
+		VecCopy(velocity, initalVel);
+		mass = particleMass;
+	}
 	Vector position;
 	Vector velocity;
+	double mass;
 };
 
 class ParticleSystem : public BaseSystem {
 public:
 	ParticleSystem(const std::string& name);
+	void generateInitalParticles(double numParticles);
+	void updateParticle(int index, Vector pos, Vector vel, double mass);
+	void updateAllVelocities(Vector velocity);
 	void getState(double* p);
 	void setState(double* p);
 	void reset(double time);
 	void display(GLenum mode = GL_RENDER);
 	int command(int argc, myCONST_SPEC char** argv);
+protected:
+	std::vector<Particle> particles;
 };
 
